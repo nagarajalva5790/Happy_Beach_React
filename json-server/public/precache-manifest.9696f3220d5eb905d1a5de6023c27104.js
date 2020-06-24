658914fa6ba3f4557edb2205e15a639ce022262e
nagarajalva5790 <30321714+nagarajalva5790@users.noreply.github.com>
2020-06-24 12:46:51 +0300

    json-server
    this is a json server, used to supply data to react application

---
+++ json-server/public/precache-manifest.9696f3220d5eb905d1a5de6023c27104.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7fa93a760b3fd25f6bf7cd68941c7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa98e4ae9c362b85c2a",
+    "url": "/static/css/2.0295d27a.chunk.css"
+  },
+  {
+    "revision": "1c7b36bebcd37dfac008",
+    "url": "/static/css/main.bc2a2813.chunk.css"
+  },
+  {
+    "revision": "aaa98e4ae9c362b85c2a",
+    "url": "/static/js/2.4dbff027.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.4dbff027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c7b36bebcd37dfac008",
+    "url": "/static/js/main.8d6aa15f.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
